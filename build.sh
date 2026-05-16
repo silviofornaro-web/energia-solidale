ceb4ec12bd3e0c9c252c9e2030e7702c34d27a17
Silvio <silviofornaro-web@users.noreply.github.com>
2026-05-16 17:29:29 +0200

    Keep Render build compatible during Streamlit switch

--- build.sh
+++ build.sh
@@ -5,3 +5,11 @@ PYTHON_BIN="${PYTHON_BIN:-python3}"
 
 "$PYTHON_BIN" -m pip install --upgrade pip
 "$PYTHON_BIN" -m pip install -r requirements.txt
+
+if [ -f requirements-django.txt ]; then
+  "$PYTHON_BIN" -m pip install -r requirements-django.txt
+fi
+
+if [ -f manage.py ]; then
+  "$PYTHON_BIN" manage.py collectstatic --no-input
+fi
