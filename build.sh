c01c3db156e0be5821ad28e85494bda20218696e
Silvio <silviofornaro-web@users.noreply.github.com>
2026-05-18 10:22:46 +0200

    Deploy Django app on Render

--- build.sh
+++ build.sh
@@ -4,11 +4,7 @@ set -o errexit
 PYTHON_BIN="${PYTHON_BIN:-python3}"
 
 "$PYTHON_BIN" -m pip install --upgrade pip
-"$PYTHON_BIN" -m pip install -r requirements.txt
-
-if [ -f requirements-django.txt ]; then
-  "$PYTHON_BIN" -m pip install -r requirements-django.txt
-fi
+"$PYTHON_BIN" -m pip install -r requirements-django.txt
 
 if [ -f manage.py ]; then
   "$PYTHON_BIN" manage.py collectstatic --no-input
