91eac0a591febb4abde1ed60101b72a45da5f891
Silvio <silviofornaro-web@users.noreply.github.com>
2026-06-03 11:21:09 +0200

    Run database setup from Render WSGI startup

--- energia_solidale_django/wsgi.py
+++ energia_solidale_django/wsgi.py
@@ -5,4 +5,18 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "energia_solidale_django.settings")
 
+
+def run_render_startup_maintenance():
+    if not os.environ.get("RENDER_EXTERNAL_HOSTNAME"):
+        return
+    if os.environ.get("SKIP_RENDER_STARTUP_MAINTENANCE", "").lower() in {"1", "true", "yes", "on"}:
+        return
+
+    from django.core.management import call_command
+
+    call_command("migrate", interactive=False, verbosity=0)
+    call_command("create_initial_superuser", verbosity=0)
+
+
 application = get_wsgi_application()
+run_render_startup_maintenance()
--- manage.py
+++ manage.py
@@ -5,14 +5,6 @@ import sys
 
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "energia_solidale_django.settings")
-    if (
-        os.environ.get("RENDER_EXTERNAL_HOSTNAME")
-        and len(sys.argv) > 1
-        and sys.argv[1] in {"migrate", "create_initial_superuser"}
-        and os.environ.get("RUN_RENDER_STARTUP_MAINTENANCE", "").lower() not in {"1", "true", "yes", "on"}
-    ):
-        print(f"Skipping {sys.argv[1]} during Render startup.")
-        return
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
